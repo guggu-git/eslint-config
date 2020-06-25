3e2b8a0bf62588b90b534d6aa98f73d7d9720fb4
Sandeep Gunduboyina <sandeep@cuemath.com>
2020-06-25 22:12:44 +0530

    v0.0.0-alpha.4

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 module.exports = {
-  extends: ["airbnb", "airbnb/hooks"]
+  parser: "babel-eslint",
+  extends: ["airbnb", "airbnb/hooks", "prettier", "prettier/react"],
+  rules: {
+    "jsx-a11y/label-has-for": 0, // deprecated but airbnb has this rule :(
+    "react/jsx-filename-extension": 0,
+    "import/prefer-default-export": 0,
+    "padding-line-between-statements": [
+      2,
+      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+      {
+        blankLine: "any",
+        prev: ["const", "let", "var"],
+        next: ["const", "let", "var"],
+      },
+    ],
+    "no-multiple-empty-lines": [2, { max: 1, maxBOF: 1 }],
+    "vars-on-top": 2,
+  },
 };
